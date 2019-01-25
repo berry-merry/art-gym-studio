1d4cc789325ba441da67c1d25797e1fc51dd22ef
jagoda.gniadek.probulska <jagoda.gniadek.probulska@gmail.com>
2019-01-25 11:07:01 +0100

    Animate sticky nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,12 @@
+
 $(document).ready(function() {
- 
+    /**** STICKY NAV ****/
+    
+    $('.js--section-what-we-do').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {offset: '60px'})
 })
